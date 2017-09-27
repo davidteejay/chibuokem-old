9ef87aed3295722f254d591e76e9f1a659bbc76f
davidteejay <chibuokem_tolu@hotmail.com>
2017-09-27 10:36:25 +0100

    Awesome!!!

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,14 @@
 $(function(){
+    //preloader
+    $(window).on('load', () => {
+		$('.preloader').slideUp();
+	});
+    
+    //Initiate WOW JS
+	new WOW().init();
     $('.parallax').parallax();
     
+    //smooth scrolling on anchor link clicks
     $(".next, .to-top").on('click', function(){
         event.preventDefault();
 
@@ -35,25 +43,23 @@ $(function(){
             .start();
     }, 3000);
     
+    //animations
     $(window).on('scroll', () => {
-        $(".determinate").each(function(){
-            var pos = $(this).offset().top;
-            var winTop = $(window).scrollTop();
-            
-            if (pos < winTop + 300) {
-                $(".html").addClass("flow-html");
-                $(".css").addClass("flow-css");
-                $(".js").addClass("flow-js");
-                $(".python").addClass("flow-python");
-                $(".php").addClass("flow-php");
-            }
-
-            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-                $('.to-top').addClass('slideup').removeClass('slidedown');
-            } else {
-                $('.to-top').addClass('slidedown').removeClass('slideup');
-            }
-        });
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            $('.to-top').addClass('slideup').removeClass('slidedown');
+        } else {
+            $('.to-top').addClass('slidedown').removeClass('slideup');
+        }
+    });
+    
+    $(".determinate").bind('inview', function(event, visible, visiblePartX, visiblePartY){
+        if(visible){
+            $(".html").addClass("flow-html");
+            $(".css").addClass("flow-css");
+            $(".js").addClass("flow-js");
+            $(".python").addClass("flow-python");
+            $(".php").addClass("flow-php");
+        }
     });
     
     $('.slider').slider({
@@ -65,4 +71,21 @@ $(function(){
         var ypos = window.pageYOffset;
         $('.parallax-img').css('top', ypos * .7 + 'px');
     });
+    
+    // Countdown
+    $('#skills .col .valign .right').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+		if (visible) {
+			$(this).find('.timer').each(function () {
+				var $this = $(this);
+				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
+					duration: 2000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.ceil(this.Counter));
+					}
+				});
+			});
+			$(this).unbind('inview');
+		}
+	})
 });
